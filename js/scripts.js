c72b20685d47533ab720723cc02a89a1772a3bbd
Kyle Bulloch & Tom Mertz <student@epicodus.com>
2015-04-15 13:58:37 -0700

    Make scripts work in html.  Hi Tom.

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,22 @@ var Ticket = {
 
 $(document).ready(function() {
 
+  $("#find-ticket").submit(function(event) {
+    event.preventDefault();
+
+    var age_bracket = $("#age_bracket").val();
+    var movie_time = $("#movie_time").val();
+    var release_year = $("#release_year").val();
+
+    var ticket = Object.create(Ticket);
+
+    ticket.age_bracket = age_bracket;
+    ticket.movie_time = movie_time;
+    ticket.release_year = release_year;
+
+    var ticket_price = ticket.getPrice();
+
+    $("#ticket-price").text(ticket_price);
+  });
+
 });
