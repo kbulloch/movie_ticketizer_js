71be4983cf74f9a91a3908e6edba83fbc5e5610d
Kyle Bulloch & Tom Mertz <student@epicodus.com>
2015-04-15 10:52:57 -0700

    Add deposit and withdraw functionality in html/js

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,34 @@ var BankAccount = {
 
 $(document).ready(function() {
 
+  var newAccount = Object.create(BankAccount);
+
   $("form#create-account").submit(function(event) {
     event.preventDefault();
 
     var new_name = $("input#name").val();
     var initial_deposit = parseInt($("input#initial-deposit").val());
 
-    var newAccount = Object.create(BankAccount);
     newAccount.name = new_name;
-    newAccount.balance += initial_deposit;
+    newAccount.balance = initial_deposit;
+
+    $("#account-balance").text(newAccount.balance);
+  });
+
+  $("form#transaction").submit(function(event) {
+    event.preventDefault();
+
+    var deposit = parseInt($("input#deposit").val());
+    var withdraw = parseInt($("input#withdraw").val());
+
+    if(!deposit) {
+      deposit = 0;
+    }
+    if(!withdraw) {
+      withdraw = 0;
+    }
+    newAccount.deposit(deposit);
+    newAccount.withdraw(withdraw);
 
     $("#account-balance").text(newAccount.balance);
   });
