7235f3787c70d2ae6537a73d008494857eadedb7
Kyle Bulloch & Tom Mertz <student@epicodus.com>
2015-04-15 10:08:46 -0700

    Add withdraw method

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,8 @@ var BankAccount = {
   balance: 0,
   deposit: function(amount) {
     this.balance += amount;
+  },
+  withdraw: function(amount) {
+    this.balance -= amount;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -5,10 +5,21 @@ describe('BankAccount',function() {
     it("will add the amount specified to the balance", function() {
       var testAccount = Object.create(BankAccount);
       testAccount.deposit(100);
-      debugger;
       expect(testAccount.balance).to.equal(100);
     });
 
   });
+
+  describe("withdraw()", function() {
+
+    it("will subtract the amount specified from the balance", function() {
+      var testAccount = Object.create(BankAccount);
+      testAccount.deposit(100);
+      testAccount.withdraw(100);
+      expect(testAccount.balance).to.equal(0);
+    });
+
+  });
+
 
 });
